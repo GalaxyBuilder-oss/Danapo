17d739f71f2c3159740a5abf9583d96807ef6a76
ajang <ajangirhas@gmaill.com>
2024-11-17 23:22:19 +0700

    Second rework: fix validasi dan beberapa tampilan

--- validation.c
+++ validation.c
@@ -8,21 +8,7 @@ char *nik_valid(char *nik, int x, int y)
     position(x, y);
     while ((input = getch()) != 13 || i == 0)
     {
-        if ((i == 0 && input == 51) || (i == 1 && input == 50))
-        {
-            position(x + i, y);
-            printf("%c", input);
-            nik1[i] = input;
-            i++;
-        }
-        else if (i >= 2 && (input >= 49 && input <= 57) && i <= 15)
-        {
-            position(x + i, y);
-            printf("%c", input);
-            nik1[i] = input;
-            i++;
-        }
-        else if (i >= 3 && (input >= 48 && input <= 57) && i <= 15)
+        if ((i == 0 && input == 51) || (i == 1 && (nik1[i-1] == 51 && input == 50)) || (i == 2 && (nik1[i-1] == 50 && input == 48)) || (i == 3 && (nik1[i-1] == 48 && input == 55)) || (i == 4 && (nik1[i-1] == 55 && input == 49)) || (i == 5 && (nik1[i-1] == 49 && input == 49)) || (i == 6 && (nik1[i-1] == 49 && (input >= 48 && input <= 51))) || (i == 7 && (nik1[i-1] == 51 && (input == 48 || input == 49))) || (i == 7 && ((nik1[i-1] == 48 || nik1[i-1] == 49 || nik1[i-1] == 50) && (input >= 48 && input <= 57))) || (i == 8 && (input >= 48 && input <= 50)) || (i == 9 && ((nik1[i-1] == 48 && input >= 48 && input <= 57) || (nik1[i-1] == 49 && input >= 48 && input <= 50))) || ((i == 10 || i == 11) && (input >= 48 && input <= 57)) || ((i >= 12 && i <= 14) && input == 48) || (i == 15 && (input >= 48 && input <= 57)))
         {
             position(x + i, y);
             printf("%c", input);
@@ -38,6 +24,7 @@ char *nik_valid(char *nik, int x, int y)
             nik1[i] = '\0';
         }
     }
+    i = 0;
     strcpy(nik, nik1);
     return nik;
 }
@@ -49,14 +36,7 @@ char *name_valid(char *name, int x, int y)
     position(x, y);
     while ((input = getch()) != 13 || i == 0)
     {
-        if ((i < 50 && input >= 65 && input <= 90))
-        {
-            position(x + i, y);
-            printf("%c", input);
-            name1[i] = input;
-            i++;
-        }
-        else if (input == 32 && i != 0 && name1[i - 1] != 32 && i != 50)
+        if (((i == 0 || name1[i-1] == 32) && (input >= 65 && input <= 90))|| (i > 1 && (name1[i-1] != 32) && input >= 97 && input <= 122) || (i == 1 && input != 32) || (i > 1 && (name1[i-1] != 32 && input == 32)) || (i == 1 && !(input >= 48 && input <= 57)))
         {
             position(x + i, y);
             printf("%c", input);
@@ -72,6 +52,7 @@ char *name_valid(char *name, int x, int y)
             name1[i] = '\0';
         }
     }
+    i = 0;
     strcpy(name, name1);
     return name;
 }
@@ -83,7 +64,7 @@ char *date_valid(char *date, int x, int y)
     position(x, y);
     while ((input = getch()) != 13 || i == 0)
     {
-        if (i < 2 && input >= 48 && input <= 57)
+        if ((i == 0 && (input >= 48 && input <= 51)) || (i == 1 && ((date1[i-1] == 49 || date1[i-1] == 50) && (input >= 48 && input <= 57))) || (i == 1 && (date1[i-1] == 51 && (input == 48 || input == 49))) || (i == 1 && (date1[i-1] == 48 && (input >= 49 && input <= 57))))
         {
             position(x + i, y);
             printf("%c", input);
@@ -112,7 +93,7 @@ char *month_valid(char *month, int x, int y)
 
     while ((input = getch()) != 13 || i == 0)
     {
-        if (input >= 48 && input <= 57 && i < 2)
+        if (i == 0 && (input == 48 || input == 49) || i == 1 && (month1[i-1] == 48 && (input >= 49 && input <= 57)) || i == 1 && (month1[i-1] == 49 && (input >= 48 && input <= 50)))
         {
             position(x + i, y);
             printf("%c", input);
@@ -141,7 +122,7 @@ char *year_valid(char *year, int x, int y)
     position(x, y);
     while ((input = getch()) != 13 || i == 0)
     {
-        if (input >= 48 && input <= 57 && i < 4)
+        if (i == 0 && (input == 49 || input == 50) || (i > 0 && i < 4) && (year1[0] == 49 && (input == 57)) || i == 1 && year1[0] == 50 && (input == 48 || input == 49) || (i > 1 && i < 4) && year1[0] == 50 && input == 48 || (i > 1 && i < 4) && year1[1] == 48 && (input >= 48 && input <= 57))
         {
             position(x + i, y);
             printf("%c", input);
@@ -236,14 +217,7 @@ char *address_valid(char address[], int x, int y)
     position(x, y);
     while ((input = getch()) != 13 || i == 0)
     {
-        if (i <= 50 && input >= 65 && input <= 90 || i != 0 && input >= 48 && input <= 57)
-        {
-            position(x + i, y);
-            printf("%c", input);
-            address1[i] = input;
-            i++;
-        }
-        else if ((input == 32 && i != 0 && address1[i - 1] != 32 && address1[i - 1] != 45 && address1[i - 1] != 46) || (input == 44 && i != 0 && address1[i - 1] != 44 && address1[i - 1] != 32 && address1[i - 1] != 45 && address1[i - 1] != 46) || (input == 45 && i != 0 && address1[i - 1] != 45 && address1[i - 1] != 32 && address1[i - 1] != 44 && address1[i - 1] != 46) || (input == 46 && i != 0 && address1[i - 1] != 46 && address1[i - 1] != 32 && address1[i - 1] != 44 && address1[i - 1] != 45))
+        if (((i == 0 || address1[i-1] == 32) && (input >= 65 && input <= 90))|| (i > 1 && (address1[i-1] != 32) && input >= 97 && input <= 122) || (i == 1 && input != 32) || (i > 1 && (address1[i-1] != 32 && input == 32)) || (i == 1 && !(input >= 48 && input <= 57)))
         {
             position(x + i, y);
             printf("%c", input);
@@ -259,6 +233,7 @@ char *address_valid(char address[], int x, int y)
             address1[i] = '\0';
         }
     }
+    i = 0;
     strcpy(address, address1);
     return address;
 }
@@ -304,14 +279,7 @@ char *phoneno_valid(char phoneno[], int x, int y)
     position(x, y);
     while ((input = getch()) != 13 || i == 0 || i < 12)
     {
-        if ((i == 1 && input == 56) || (i == 2 && phoneno1[i - 1] == 56 && input != 54) || (i == 2 && phoneno1[i - 1] == 8 && input != 52) || (i == 2 && input == 49) || (i == 2 && input == 50) || (i == 2 && input == 51) || (i == 2 && input == 53) || (i == 2 && input == 53) || (i == 2 && input == 55) || (i == 2 && input == 57) || (i == 2 && input == 56) && (i > 2 && input >= 48 && input <= 57))
-        {
-            position(x + i, y);
-            printf("%c", input);
-            phoneno1[i] = input;
-            i++;
-        }
-        else if (i <= 12 || (i == 0 && input == 48))
+        if (((i > 2 && i < 12) && (input >= 48 && input <= 57)) || (i == 0 && input == 48) || (i == 1 && input == 56) || (i == 2 && input != 57 && input != 48 && input != 50 && input != 52 && input != 54))
         {
             position(x + i, y);
             printf("%c", input);
@@ -327,7 +295,6 @@ char *phoneno_valid(char phoneno[], int x, int y)
             phoneno1[i] = '\0';
         }
     }
-    i = 0;
     strcpy(phoneno, phoneno1);
     return phoneno;
 }
